423fdb0491479ae63ec8aff23e4504cc5e5438fa
LAPTOP-VTUENU7O\sebas <jsalarcon74@misena.edu.co>
2021-09-24 16:29:49 -0500

    APP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import foto from './pages/Error/Errorxd.png'
+import React from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHome} from '@fortawesome/free-solid-svg-icons'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
+import Home from './pages/home/Home';
+import Portafolio from './pages/portafolio/Portafolio';
+import {
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <Router>
+    <Switch>
+    <Route exact path="/">
+
+    </Route>
+    <Route path="/portafolio">
+    <Portafolio></Portafolio>
+
+    </Route>
+    <Route path="/home">
+    <Home></Home>
+
+    </Route>
+    <Route path="*">
+    <Link to="/home"> <button className="seccion__button"><FontAwesomeIcon icon={faHome}/></button></Link>
+    <div className="errorxddx">
+      <img src={foto} width="1000px" height="550px"></img>
     </div>
+    </Route>
+    
+    </Switch>
+  </Router>
   );
 }
 
