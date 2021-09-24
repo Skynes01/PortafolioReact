391ecbda041616b231544a9bb759cb5721b6170b
LAPTOP-VTUENU7O\sebas <jsalarcon74@misena.edu.co>
2021-09-24 16:57:17 -0500

    ErrorJS

---
+++ src/pages/Error/Error404.js
@@ -0,0 +1,14 @@
+import foto from './Errorxd.png'
+
+function Error404() {
+	return(
+        
+		<div className="foterxd">
+
+        <img src={foto} width="880px" height="193px"> 
+        </img>
+        </div>
+		);
+}
+
+export default Error404;
